fdaae88befe39803b4a51817279cfa02d0a75d0d
谢丰年 <xieniangao@126.com>
2018-04-20 01:03:58 +0800

    update : fixed mistake

--- class4-使用继承改进框架/demo.js
+++ class4-使用继承改进框架/demo.js
@@ -1,33 +1,31 @@
 var init = function(selector){
-	//砍头函数
+    //砍头函数
     var removeStringFirst = function(str){
-		var charArr = str.split('');
-    	charArr.shift();
-		return charArr.join('');
-	}
+        var charArr = str.split('');
+        charArr.shift();
+        return charArr.join('');
+    }
 
     //this.dom初始化为数组，带有数组全部方法
     this.dom = [];
+    var me = this; //使用me这个本地变量捕捉init的this，防止在call方法中丢失this
     var firstChar = selector[0];    //获取首字符
     if(firstChar == '.'){           //实现class选择器
-    	selector = removeStringFirst(selector);
-    	var dom = document.getElementsByClassName(selector);
+        selector = removeStringFirst(selector);
+        var dom = document.getElementsByClassName(selector);
         Array.prototype.forEach.call(dom,function(item){
-        	this.dom.push(item);
+            me.dom.push(item);
         })
-        console.log(this.dom)
     }
     else if(firstChar == '#'){       //实现id选择器
-    	selector = removeStringFirst(selector);
-    	this.dom.push(document.getElementById(selector));
-    	console.log(this.dom);
+        selector = removeStringFirst(selector);
+        me.dom.push(document.getElementById(selector));
     }
     else {                           //实现标签选择器
         var dom = document.getElementsByTagName(selector);
         Array.prototype.forEach.call(dom,function(item){
-        	this.dom.push(item);
+            me.dom.push(item);
         })
-        console.log(this.dom);
     }
 }
 init.prototype = {
